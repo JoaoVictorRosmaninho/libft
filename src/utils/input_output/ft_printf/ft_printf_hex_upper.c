01207ef7d6c9dd983285380d945eaebf2cff7b45
joaovictorrosmaninho <joaovictorrosmaninho@gmail.com>
2024-11-17 15:24:38 -0300

    fix: functions command line params

--- src/utils/input_output/ft_printf/ft_printf_hex_upper.c
+++ src/utils/input_output/ft_printf/ft_printf_hex_upper.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_hex_upper.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jv <jv@student.42.fr>                      +#+  +:+       +#+        */
+/*   By: joao <joao@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 23:42:10 by jv                #+#    #+#             */
-/*   Updated: 2022/11/02 11:56:45 by jv               ###   ########.fr       */
+/*   Updated: 2024/11/17 15:24:14 by joao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 ssize_t	ft_printf_hex_upper(int n)
 {
 	ssize_t	len;
-	char	*number;
+	char	number[128] = { 0 };
 
 	len = 0;
 	if (!n)
 		len = ft_putstr_fd("0", 1);
 	else
 	{
-		number = int2hex(n, 1);
+		int2hex(n, 1, number);
 		len += ft_putstr_fd(number, 1);
-		free(number);
 	}
 	return (len);
 }
